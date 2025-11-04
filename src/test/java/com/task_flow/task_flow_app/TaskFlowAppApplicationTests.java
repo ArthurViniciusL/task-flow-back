313b381e6edff39778fcbda506c75f172a4f10ee
arthurviniciussl <arthurviniciussl.contato@gmail.com>
2025-11-04 20:33:01 -0300

    initial setup

---
+++ src/test/java/com/task_flow/task_flow_app/TaskFlowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task_flow.task_flow_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFlowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
